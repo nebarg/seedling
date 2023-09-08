a1b0253e3b42c38217fd0fc940a20cfdd416a085
Grant Bennett <subs@woode.co.uk>
2023-09-08 16:33:50 +0100

    Ugly block most console commands

---
+++ app/Exceptions/InvalidConsoleCommandException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Symfony\Component\Console\Exception\ExceptionInterface;
+
+class InvalidConsoleCommandException extends Exception implements ExceptionInterface
+{
+    //
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,45 @@
 
 namespace App\Providers;
 
+use App\Exceptions\InvalidConsoleCommandException;
+use Illuminate\Console\Events\CommandStarting;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use RuntimeException;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    private array $enabledCommands = [
+        'app:seed',
+        'db:seed',
+        'make:factory',
+        'make:model',
+        'make:seed',
+        'make:seeder',
+        'make:command',
+        /** Required for composer */
+        'package:discover',
+        'vendor:publish',
+    ];
+
     public function register(): void
     {
-        //
+        if (! App::isLocal()) {
+            throw new RuntimeException('This application can only run locally');
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Event::listen(CommandStarting::class, function($event) {
+            $command = $event->input->getArgument('command') ?? $event->input->getArguments()[0];
+
+            if (!in_array($command, $this->enabledCommands, true)) {
+                throw new InvalidConsoleCommandException(
+                    'Standard console commands are disabled. Use `app:seed` to seed data'
+                );
+            }
+        });
     }
 }
