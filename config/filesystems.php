40456455170ea62def5dbb12fa34ff7f8fc07657
Grant Bennett <subs@woode.co.uk>
2023-09-08 15:44:49 +0100

    clean up

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,29 +30,9 @@ return [
 
     'disks' => [
 
-        'local' => [
+        'database' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
-            'throw' => false,
-        ],
-
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
-        ],
-
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'root' => base_path('database'),
             'throw' => false,
         ],
 
