1695fba3792695a367c079f954d5f56dbf14f078
Grant Bennett <subs@woode.co.uk>
2023-09-08 16:43:52 +0100

    Create basic seed command

---
+++ app/Console/Commands/SeedDatabaseCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Laravel\Prompts;
+
+class SeedDatabaseCommand extends Command
+{
+    protected $signature = 'app:seed';
+
+    protected $description = 'Populate your database with fake data.'
+        . ' This application does not handle migrations.';
+
+    public function handle(): void
+    {
+        $availableSeeders = $this->findSeeders();
+
+        $seedsToSow = $this->promptUser($availableSeeders);
+
+        if ($seedsToSow === 'Cancel') {
+            $this->info('No seeds have been sown.');
+
+            return;
+        }
+
+        if ($seedsToSow === 'All') {
+            $seedsToSow = $availableSeeders;
+        }
+
+        $this->seed($seedsToSow);
+    }
+
+    private function findSeeders(): array
+    {
+        $files = Storage::disk('database')->allFiles('seeders');
+
+        return array_map(static function ($seeder) {
+            return implode(
+                ' ',
+                Str::ucsplit(
+                    basename($seeder, '.php')
+                )
+            );
+        }, $files);
+    }
+
+    private function promptUser(array $files): string
+    {
+        return Prompts\select(
+            label: 'What seeds would you like to sow today?',
+            options: ['All', ...$files, 'Cancel'],
+            default: 'Cancel'
+        );
+    }
+
+    private function seed(string|array $seeders): void
+    {
+        foreach ((array) $seeders as $seeder) {
+            $filename = str_replace(' ', '', $seeder);
+
+            try {
+                Artisan::call('db:seed', ['--class' => $filename]);
+
+                $this->info('Successfully sown ' . $seeder);
+            } catch (Exception) {
+                $this->error(sprintf('Sowing %s failed', $seeder));
+            }
+        }
+    }
+}
