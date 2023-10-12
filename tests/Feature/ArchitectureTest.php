15beb5e038f6c622c5704cd988a49be95d1894f2
Grant Bennett <subs@woode.co.uk>
2023-10-12 23:26:10 +0100

    Add a couple of arch tests

--- tests/Feature/ArchitectureTest.php
+++ tests/Feature/ArchitectureTest.php
@@ -1,9 +1,28 @@
 <?php
 
-test('Seeders ')
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Seeder;
+
+test('Database Dumps directory exists and is readable')
+    ->expect('database/dumps')
+    ->toBeReadableDirectory();
+
+test('Database Factories directory exists and is readable')
+    ->expect('database/factories')
+    ->toBeReadableDirectory();
+
+test('Database Seeders directory exists and is readable')
+    ->expect('database/seeders')
+    ->toBeReadableDirectory();
+
+test('Seeders must extend the framework seeder class')
     ->expect('Database\Seeders')
-    ->toExtend('Illuminate\Database\Seeder');
+    ->toExtend(Seeder::class);
+
+test('Factories must extend the framework factory class')
+    ->expect('Database\Factories')
+    ->toExtend(Factory::class);
 
 test('No forgotten dumps, exits or dev tools')
-    ->expect(['dd', 'dump', 'ray', 'var_dump', 'exit'])
+    ->expect(['dd', 'dump', 'var_dump', 'ray', 'die', 'exit'])
     ->not->toBeUsed();
